2aad8b66b7ee2c1e94e37216cabd6ca3d8224b51
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-23 20:36:22 +0800

    virtio_arm64only: Add `com.android.ranging` and `uprobestats` to EPPE allow list
    Change-Id: I2efc84a617b4674f72c5d8947eadb89c058f88b7

--- aosp_virtio_arm64only_16k.mk
+++ aosp_virtio_arm64only_16k.mk
@@ -15,8 +15,10 @@ $(call inherit-product, device/virt/virtio_arm64only/device.mk)
 
 $(call enforce-product-packages-exist,\
     android.hardware.health@2.0-impl-default.recovery \
+    com.android.ranging \
     DeviceDiagnostics \
-    product_manifest.xml)
+    product_manifest.xml \
+    uprobestats)
 
 PRODUCT_NAME := aosp_virtio_arm64only_16k
 PRODUCT_DEVICE := virtio_arm64only_16k
