300cddf1049b1206bf07e16320f2722e3ea0b774
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-14 23:52:25 +0800

    virtio_arm64only: Add Android Go build targets
    Change-Id: I241cc5139dc3c0c477690b194a9280c926ac5c2e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,11 +7,15 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_virtio_arm64only.mk \
     $(LOCAL_DIR)/aosp_virtio_arm64only_16k.mk \
+    $(LOCAL_DIR)/aosp_virtio_arm64only_go.mk \
     $(LOCAL_DIR)/lineage_virtio_arm64only.mk \
-    $(LOCAL_DIR)/lineage_virtio_arm64only_16k.mk
+    $(LOCAL_DIR)/lineage_virtio_arm64only_16k.mk \
+    $(LOCAL_DIR)/lineage_virtio_arm64only_go.mk
 
 $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_virtio_arm64only-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_virtio_arm64only_16k-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += aosp_virtio_arm64only_go-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only-$(build_type)) \
-    $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only_16k-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only_16k-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only_go-$(build_type)))
